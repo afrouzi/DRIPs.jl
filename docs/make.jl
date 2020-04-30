d332981bc23081b58a604ff0e77b4c7611edc1ee
Hassan Afrouzi <hassan@afrouzi.com>
2020-04-30 03:53:12 -0400

    fixed makedocs

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,14 @@
 using Documenter, DRIPs
 
-# makedocs(
-#     modules  = [DRIPs],
-#     format = Documenter.HTML(prettyurls = "--local" ∉ ARGS),
-#     sitename = "DRIPs.jl",
-#     pages    = Any[
-#         "Home" => "index.md",
-#        ]
-#     )
-makedocs()
+makedocs(
+    modules  = [DRIPs],
+    format = Documenter.HTML(prettyurls = "--local" ∉ ARGS),
+    sitename = "DRIPs.jl",
+    pages    = Any[
+        "Home" => "index.md",
+       ]
+    )
+#makedocs()
 
 deploydocs(
     repo   = "github.com/afrouzi/DRIPs.jl.git",
